78bef2d8dacab93a1933e5f4457748bd25ebae52
sayalidamal1999 <121383251+sayalidamal1999@users.noreply.github.com>
2023-04-12 21:22:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
+	// Convert the input string to lowercase and remove non-alphanumeric characters
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  // Loop through the string and compare characters from both ends
+  for (let i = 0, j = str.length - 1; i < j; i++, j--) {
+    if (str[i] !== str[j]) {
+      return false;
+    }
+  }
+  return true;
 
 }
 module.exports = palindrome
